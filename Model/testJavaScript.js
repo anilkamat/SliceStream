33c279d9fdeef2bfbd96bae222e8f1d7960abf8a
vincent31545 <122557562+vincent31545@users.noreply.github.com>
2023-10-10 17:45:49 -0400

    image capture
    captures the image for the video

--- Model/testJavaScript.js
+++ Model/testJavaScript.js
@@ -17,6 +17,21 @@ function handleVideoUpload() {
 // Add an event listener to the file input
 document.getElementById('videoInput').addEventListener('change', handleVideoUpload);
 
+// sets it to the penguinExample video
 function setVideo() {
     document.getElementById("userVideo").src="./penguinExample.mp4";
 }
+
+
+//
+function capture() {
+    var canvas = document.getElementById("canvas");     
+    var video = document.getElementById("userVideo");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
+    canvas.toBlob() = (blob) => {
+      const img = new Image();
+      img.src = window.URL.createObjectUrl(blob);
+    };
+  }
