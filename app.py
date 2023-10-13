e11170049cb8e24026cc32a6ff314d4b52b28f6d
orgwirelist123 <orgwirelist123@gmail.com>
2023-10-13 16:56:03 -0400

    app.py now generates autosegmented masks for images

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+import numpy as np
+import matplotlib.pyplot as plt
 import cv2
+import tkinter
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
 
 checkpoint_path = "./models/sam_vit_h_4b8939.pth"
 image_path = "./images/car.jpg"
@@ -9,6 +27,17 @@ image = cv2.imread(image_path)
 
 sam = sam_model_registry[model_type](checkpoint=checkpoint_path)
 mask_generator = SamAutomaticMaskGenerator(sam)
+
+print("Generating masks...")
 masks = mask_generator.generate(image)
+print("Masks generated!")
 
 print(masks)
+
+print("Displaying image with masks...")
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.show() 
+print("Image with masks displayed!")
