497ecdd7e0a31e7257b7d3e621b716cf23e85854
vincent31545 <vincentlau31545@gmail.com>
2023-10-17 17:24:54 -0400

    made the video player work
    the process I did is in the videoReactDocumentation.txt
    this lets you upload a video from your local machine.
    I need to restyle some buttons and make the capture button work.

--- app/page.js
+++ app/page.js
@@ -1,28 +1,45 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
+"use client";
+import { useState } from "react";
+import styles from './page.module.css';
 
 export default function Home() {
+  const [src, setSrc] = useState(""); // Initialize source URL as empty string
+
+  const handleChange = (event) => {
+    try {
+      const file = event.target.files[0];
+      setSrc(URL.createObjectURL(file));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.websiteName}>
-        Slice Stream
-      </div>
+      <div className={styles.websiteName}>Slice Stream</div>
 
       <div className={styles.videoPlayer}>
-        <video controls width="700" height="400">
-          <source src="./penguinExample.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        {src ? ( // Conditional rendering based on whether src is set
+          <video controls width="700" height="400">
+            <source src={src} type="video/mp4" />
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        ) : (
+          <p>Select a video file</p>
+        )}
+        
       </div>
-      
+
       <div className={styles.buttonContainer}>
         <div className={styles.button}>
-          <div className={styles.span}>
-            Select Something
-          </div>
+          <div className={styles.span}>Select Video</div>
+        </div>
+        <input type="file" accept="video/mp4" onChange={handleChange} />
+        <div className={styles.button}>
+          <div className={styles.span}>Capture Video</div>
         </div>
       </div>
     </main>
-    
-  )
+  );
 }
